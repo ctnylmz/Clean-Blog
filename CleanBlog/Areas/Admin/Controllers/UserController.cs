88451fb56c880c91b51f10d474b746998ea7f803
ctnylmz <143901863+ctnylmz@users.noreply.github.com>
2023-09-26 15:14:43 +0300

    Asp.Net Core 6 MVC (Login Message)

--- CleanBlog/Areas/Admin/Controllers/UserController.cs
+++ CleanBlog/Areas/Admin/Controllers/UserController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using CleanBlog.Models;
+using CleanBlog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;   
+        private readonly SignInManager<ApplicationUser> _signInManager;   
+        private readonly INotyfService _notification;
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, INotyfService notification)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _notification = notification;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,7 +29,38 @@ namespace CleanBlog.Areas.Admin.Controllers
         [HttpGet("Login")]
         public IActionResult Login()
         {
-            return View();
+            return View(new LoginVM());
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var existingUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == vm.Username);
+
+            if (existingUser == null)
+            {
+                _notification.Error("Kullanıcı Adı Bulunamadı!");
+                return View(vm);
+
+            }
+
+            var verifyPassword = await _userManager.CheckPasswordAsync(existingUser,vm.Password);
+
+            if (!verifyPassword)
+            {
+                _notification.Error("Şifre Hatalı!");
+                return View(vm);
+            }
+
+        
+            await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
+
+            return RedirectToAction("Index","User",new {area="Admin"});
         }
     }
 }
--- CleanBlog/Program.cs
+++ CleanBlog/Program.cs
@@ -1,3 +1,5 @@
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 using CleanBlog.Data;
 using CleanBlog.Models;
 using CleanBlog.Utilites;
@@ -18,6 +20,8 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 builder.Services.AddScoped<IDbInitializer,DbInitializer>();
 
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 10; config.IsDismissable = true; config.Position = NotyfPosition.BottomRight; });
+
 var app = builder.Build();
 DataSeeding();
 
@@ -27,10 +31,14 @@ if (!app.Environment.IsDevelopment())
 	app.UseHsts();
 }
 
+app.UseNotyf();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
