d396240fa9b5939bf718093f35ced8b4c6ae5502
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-10-31 19:52:13 +0300

    Home And About Page

---
+++ Clean_Blog/Clean_Blog/Controllers/PopularController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Blog.Controllers
+{
+    public class PopularController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
