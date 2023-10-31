431b19becabd60b2d54dafae7cc83caf66bfb541
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-10-31 20:19:00 +0300

    Admin Panel

---
+++ Clean_Blog/Clean_Blog/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Blog.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        [Route("/Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
