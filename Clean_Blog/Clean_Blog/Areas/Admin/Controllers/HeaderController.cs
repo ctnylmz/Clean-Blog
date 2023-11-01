c5c4d099c9d0966b11bb3c4562353fde53b8ca77
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-11-01 08:49:48 +0300

    Save Database

---
+++ Clean_Blog/Clean_Blog/Areas/Admin/Controllers/HeaderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Blog.Areas.Admin.Controllers
+{
+    public class HeaderController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Clean_Blog/Clean_Blog/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Clean_Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Clean_Blog.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Header> Headers { get; set; }
+    }
+}
