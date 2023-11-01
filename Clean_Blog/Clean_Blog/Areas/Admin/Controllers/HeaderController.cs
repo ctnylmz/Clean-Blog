166e7c364b1a70726f040d9a51955ca961e63597
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-11-01 10:05:52 +0300

    Admin Panel Header Page

--- Clean_Blog/Clean_Blog/Areas/Admin/Controllers/HeaderController.cs
+++ Clean_Blog/Clean_Blog/Areas/Admin/Controllers/HeaderController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clean_Blog.Data;
+using Clean_Blog.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Clean_Blog.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HeaderController : Controller
     {
-        [Area("Admin")]
+        private readonly ApplicationDbContext _context;
+
+        public HeaderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var header = _context.Headers.Find(1);
+
+            return View(header);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Header header)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Headers.Update(header);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(header);
         }
     }
 }
